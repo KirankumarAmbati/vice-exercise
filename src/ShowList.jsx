75723a9e0238a7ba7934bfa1f9df61d12ea0438e
cmackenz <colin.mackenzie@mlb.com>
2018-03-30 21:55:45 -0400

    Finishes styles

--- src/ShowList.jsx
+++ src/ShowList.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions, jsx-a11y/no-noninteractive-element-interactions */
-// TODO clean up
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import PropTypes from 'prop-types';
 
